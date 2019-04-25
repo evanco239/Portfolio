6d139d707097fc6f370207ecbf86cb3b9365181d
evanco239 <evanco239@gmail.com>
2019-04-25 13:13:38 -0400

    Updated all sections
    Fully responsive with hamburger menu. deploying and checking compatibility across browsers
    
    To do: attach documents for download

---
+++ JS/main.js
@@ -0,0 +1,29 @@
+const navbar = document.querySelector('#navbar')
+const pageDown = document.querySelector('#page-down')
+const hamburger = document.querySelector('#hamburger')
+const mobileLinks = document.querySelector('#mobile-nav-links')
+
+// capture scroll event  to attach sticky nav
+window.onscroll = () =>{
+    if (window.pageYOffset > 140) {
+        navbar.classList.remove("sticky");
+        pageDown.classList.add("sticky");
+      } else {
+        navbar.classList.add("sticky");
+        pageDown.classList.remove("sticky");
+      }
+}
+
+// toggle display class on hamburger menu
+hamburger.addEventListener('click', () => {
+    if (mobileLinks.style.display == "none") {
+        mobileLinks.style.display = "block"
+    } else {
+        mobileLinks.style.display = "none"
+    }
+})
+
+// close the hamburger menu on click for link
+mobileLinks.addEventListener('click', () => {
+    mobileLinks.style.display = "none"
+})
