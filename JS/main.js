0b9523070ef648604bd082d17549866da8d3cd46
evanco239 <evanco239@gmail.com>
2019-04-25 13:22:30 -0400

    Updated Styling and javascript hamburger function
    Updated zindex of navbar (contact form was above it).
    
    updated JS to make sure the display case was selected on the first click.

--- JS/main.js
+++ JS/main.js
@@ -1,29 +1,30 @@
-const navbar = document.querySelector('#navbar')
-const pageDown = document.querySelector('#page-down')
-const hamburger = document.querySelector('#hamburger')
-const mobileLinks = document.querySelector('#mobile-nav-links')
+const navbar = document.querySelector("#navbar");
+const pageDown = document.querySelector("#page-down");
+const hamburger = document.querySelector("#hamburger");
+const mobileLinks = document.querySelector("#mobile-nav-links");
 
 // capture scroll event  to attach sticky nav
-window.onscroll = () =>{
-    if (window.pageYOffset > 140) {
-        navbar.classList.remove("sticky");
-        pageDown.classList.add("sticky");
-      } else {
-        navbar.classList.add("sticky");
-        pageDown.classList.remove("sticky");
-      }
-}
+window.onscroll = () => {
+  if (window.pageYOffset > 140) {
+    navbar.classList.remove("sticky");
+    pageDown.classList.add("sticky");
+  } else {
+    navbar.classList.add("sticky");
+    pageDown.classList.remove("sticky");
+  }
+};
 
 // toggle display class on hamburger menu
-hamburger.addEventListener('click', () => {
-    if (mobileLinks.style.display == "none") {
-        mobileLinks.style.display = "block"
-    } else {
-        mobileLinks.style.display = "none"
-    }
-})
+hamburger.addEventListener("click", () => {
+  console.log(mobileLinks.style.display);
+  if (mobileLinks.style.display == "none" || mobileLinks.style.display == "") {
+    mobileLinks.style.display = "block";
+  } else {
+    mobileLinks.style.display = "none";
+  }
+});
 
 // close the hamburger menu on click for link
-mobileLinks.addEventListener('click', () => {
-    mobileLinks.style.display = "none"
-})
+mobileLinks.addEventListener("click", () => {
+  mobileLinks.style.display = "none";
+});
